03fbd829e5a8b5dc3a26506cbd52998cf2f1a364
bswim777@gmail.com <bswim777@gmail.com@084922cd-2ddb-2a8f-f8cd-eacabf4347da>
2010-10-15 00:45:56 +0000

    Updated Admin CP Sidebar to include stats & users online widgets

--- clancms/views/admincp/sidebar.php
+++ clancms/views/admincp/sidebar.php
@@ -1,3 +1,7 @@
 <div id="sidebar">
 	<?php $this->load->widget(ADMINCP . 'login'); ?>
+	<div class="space"></div>
+	<?php $this->load->widget(ADMINCP . 'stats'); ?>
+	<div class="space"></div>
+	<?php $this->load->widget(ADMINCP . 'users_online'); ?>
 </div>
