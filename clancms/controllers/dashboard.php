872f0577de561bdfe6826c66597f5720d2aa80c9
bswim777@gmail.com <bswim777@gmail.com@084922cd-2ddb-2a8f-f8cd-eacabf4347da>
2011-05-07 05:31:12 +0000

    Fixed squad articles title

--- clancms/controllers/dashboard.php
+++ clancms/controllers/dashboard.php
@@ -93,7 +93,7 @@ class Dashboard extends CI_Controller {
 					}
 				
 					// Assign article information
-					$slide->slider_title = $article->squad . $article->article_title;
+					$slide->slider_title = $article->squad . ': ' . $article->article_title;
 					
 					// Check if the slide has content
 					if(!$slide->slider_content)
