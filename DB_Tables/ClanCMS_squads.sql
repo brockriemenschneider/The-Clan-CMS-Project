76f7c6803b3583883aea2615a9a385d25dc47ed3
Travis "FuntimeError" Baker <basslingo@gmail.com>
2012-02-11 14:03:34 -0600

    Spring Cleaning ;)

--- DB_Tables/ClanCMS_squads.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `ClanCMS_squads` ADD `squad_icon` VARCHAR( 64 ) NOT NULL AFTER `squad_priority` 
--- DB_Tables/ClanCMS_users.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `ClanCMS_users` ADD `can_shout` TINYINT( 1 ) NOT NULL DEFAULT '1',
-ADD `can_upload` TINYINT( 1 ) NOT NULL DEFAULT '1',
-ADD `has_voice` TINYINT( 1 ) NOT NULL DEFAULT '1'
