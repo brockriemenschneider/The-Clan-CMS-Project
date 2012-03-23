f9383a549d236c9068c348836417fda1b98b019a
Mahngiel <mahngiel@gmail.com>
2012-03-22 22:23:48 -0300

    set install to true

--- clancms/models/clancms.php
+++ clancms/models/clancms.php
@@ -46,7 +46,7 @@ class ClanCMS extends CI_Model {
 		$CI->load->model('Tracker_model', 'tracker');
 		
 		// Define Install
-		define('INSTALL', FALSE);
+		define('INSTALL', TRUE);
 		
 		// Check if we are installing
 		if(!INSTALL)
